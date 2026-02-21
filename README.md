2c4993ea062d0e3560c59ed0974ae4858445e109
scort <7415450+scorttt@users.noreply.github.com>
2026-02-20 20:05:33 -0800

    v2 idk if this will just work lol
    regression: no more cat :( i cant figure it out

--- README.md
+++
@@ -1,1 +0,0 @@
-scort.cc
---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://scort.cc',
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    
+    integrations: [icon()]
+});
