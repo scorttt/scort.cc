00a57ac08215e860f39dba7874c69a1f880aeacb
scort <7415450+scorttt@users.noreply.github.com>
2026-02-28 05:09:19 -0800

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://scort.cc',
-    output: "server",
+    // output: "server",
     vite: {
         plugins: [tailwindcss()]
     },
